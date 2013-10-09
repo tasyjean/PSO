011368f45b49b70f92ff8ec76421ea228de9dfe9
Fei Wang <nriverwang@gmail.com>
2013-10-09 21:21:58 +0800

    Update Evaluation.java

--- src/pso/core/Evaluation.java
+++ src/pso/core/Evaluation.java
@@ -2,6 +2,10 @@ package pso.core;
 
 import pso.type.Calculable;
 
+/** 
+ * All evaluation functions shoulde extends class Evaluation and 
+ * the evaluate() method.
+ */
 public abstract class Evaluation {
   public abstract Calculable evaluate(Calculable[] position);
 }
